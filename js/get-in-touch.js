9ea31861a76678bb48924fa25f10d2c68c8529b8
Stephen Whittaker <stephenwhittaker23@gmail.com>
2019-10-04 21:13:32 +0100

    pressing submit now alerts with array of all inputs

--- js/get-in-touch.js
+++ js/get-in-touch.js
@@ -1,5 +1,24 @@
-const submitButtonClick = document.getElementById("submitButton");
+let submitButtonClick = document.getElementById("submitButton");
 console.log('whatevs');
+/*let formName = document.getElementById("name").value;
+let formEmail = document.getElementById("email").value;
+let formTel = document.getElementById("phone").value;
+let formMessage = document.getElementById("messageText").value;
+let formDataContent = [];*/
+console.log(submitButtonClick);
+let formDataContent = [];
 
+const formData = function() {
+    let formName = document.getElementById("name").value;
+let formEmail = document.getElementById("email").value;
+let formTel = document.getElementById("phone").value;
+let formMessage = document.getElementById("messageText").value;
 
-submitButtonClick.addEventListener('click', function(){alert("Your message has been submitted. Really. I mean it.");},false);
+    if (formName != null) {formDataContent.push(formName)};
+    if (formEmail != null ) {formDataContent.push(formEmail)};
+    if (formTel != null) {formDataContent.push(formTel)};
+    if (formMessage != null) {formDataContent.push(formMessage)};
+    alert(`Your message has been submitted. Here's what you've submitted: ${formDataContent}`);
+};
+
+submitButtonClick.addEventListener('click',formData,false);
