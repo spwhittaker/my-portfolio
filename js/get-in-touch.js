400dad55340c385bf5a5e580236ec597583fd56e
Stephen Whittaker <stephenwhittaker23@gmail.com>
2019-10-04 19:43:46 +0100

    added js file for form

---
+++ js/get-in-touch.js
@@ -0,0 +1,5 @@
+const submitButton = document.getElementById("submitButton");
+console.log('whatevs');
+
+
+submitButton.addEventListener('click', alert("Your message has been submitted. Really. I mean it."),false);
