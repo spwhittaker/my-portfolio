7b569be54bd1437f37469c81dcb3cee5a92599fe
Stephen Whittaker <stephenwhittaker23@gmail.com>
2019-10-05 11:01:14 +0100

    Fixed nav typo

--- js/get-in-touch.js
+++ js/get-in-touch.js
@@ -8,17 +8,18 @@ let formDataContent = [];*/
 console.log(submitButtonClick);
 let formDataContent = [];
 
+
 const formData = function() {
     let formName = document.getElementById("name").value;
 let formEmail = document.getElementById("email").value;
 let formTel = document.getElementById("phone").value;
 let formMessage = document.getElementById("messageText").value;
 
-    if (formName != null) {formDataContent.push(formName)};
-    if (formEmail != null ) {formDataContent.push(formEmail)};
-    if (formTel != null) {formDataContent.push(formTel)};
-    if (formMessage != null) {formDataContent.push(formMessage)};
-    alert(`Your message has been submitted. Here's what you've submitted: ${formDataContent}`);
+    if (formName != null) {formDataContent.push("\n\tName: " + formName)};
+    if (formEmail != null ) {formDataContent.push("\n\tEmail: " + formEmail)};
+    if (formTel != null) {formDataContent.push("\n\tPhone Number: " + formTel)};
+    if (formMessage != null) {formDataContent.push("\n\tYour message: " + formMessage)};
+    alert("Your message has been submitted. Here's what you've submitted: \n" + formDataContent + "\nThis message is actually going somewhere. I promise.");
 };
 
 submitButtonClick.addEventListener('click',formData,false);
