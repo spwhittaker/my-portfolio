db12fb6828dcea2a138f7560d182b215937d9094
Stephen Whittaker <stephenwhittaker23@gmail.com>
2019-10-06 20:15:52 +0100

    added clear button and event listener

--- js/get-in-touch.js
+++ js/get-in-touch.js
@@ -1,25 +1,41 @@
 let submitButtonClick = document.getElementById("submitButton");
-console.log('whatevs');
+let clearButtonClick  = document.getElementById("clearButton");
+console.log('TEST');
 /*let formName = document.getElementById("name").value;
 let formEmail = document.getElementById("email").value;
 let formTel = document.getElementById("phone").value;
 let formMessage = document.getElementById("messageText").value;
 let formDataContent = [];*/
-console.log(submitButtonClick);
+// console.log(submitButtonClick);
 let formDataContent = [];
+let submittedData = [];
 
 
-const formData = function() {
+const formData = function () {
     let formName = document.getElementById("name").value;
-let formEmail = document.getElementById("email").value;
-let formTel = document.getElementById("phone").value;
-let formMessage = document.getElementById("messageText").value;
+    let formEmail = document.getElementById("email").value;
+    let formTel = document.getElementById("phone").value;
+    let formMessage = document.getElementById("messageText").value;
 
-    if (formName != null) {formDataContent.push("\n\tName: " + formName)};
-    if (formEmail != null ) {formDataContent.push("\n\tEmail: " + formEmail)};
-    if (formTel != null) {formDataContent.push("\n\tPhone Number: " + formTel)};
-    if (formMessage != null) {formDataContent.push("\n\tYour message: " + formMessage)};
+    if (formName != null) {
+        formDataContent.push("\n\tName: " + formName)
+    };
+    if (formEmail != null) {
+        formDataContent.push("\n\tEmail: " + formEmail)
+    };
+    if (formTel != null) {
+        formDataContent.push("\n\tPhone Number: " + formTel)
+    };
+    if (formMessage != null) {
+        formDataContent.push("\n\tYour message: " + formMessage)
+    };
     alert("Your message has been submitted. Here's what you've submitted: \n" + formDataContent + "\nThis message is actually going somewhere. I promise.");
+    submittedData = formDataContent;
+    formDataContent = [];
+
 };
 
-submitButtonClick.addEventListener('click',formData,false);
+const clearData = function() {formDataContent = [];};
+
+submitButtonClick.addEventListener('click', formData, false);
+clearButtonClick.addEventListener('click', clearData, false);
